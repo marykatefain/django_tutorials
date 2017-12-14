3604b06f9beeb07e7f817a2e3ec77b5aaf156444
Mary Kate Fain <mkfain1@gmail.com>
2017-12-14 13:59:38 -0500

    view test for future and past qs

--- polls/tests.py
+++ polls/tests.py
@@ -54,11 +54,11 @@ class QuestionIndexViewTests(TestCase):
         """
         Questions with a pub_date in the past are displayed on the index page.
         """
-        create_question(question_text="Past question", days=-30)
+        create_question(question_text="Past question.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            ['<Question: Past question>']
+            ['<Question: Past question.>']
         )
 
     def test_future_question(self):
@@ -69,3 +69,15 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both future and past questions exist, only display past qestions on index.
+        """
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future Question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
